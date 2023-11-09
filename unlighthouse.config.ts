e68e3867ec353df68221ab0530f2ec8028d1fbb4
Levin Keller <post@levinkeller.de>
2023-11-09 16:03:10 +0000

    feat: enforce min values for web vitals

--- unlighthouse.config.ts
+++ unlighthouse.config.ts
@@ -1,4 +1,14 @@
 export default {
-chrome: {
-    useSystem    : false }
+  chrome: {
+    useSystem: false,
+  },
+  site: 'https://www.kulturland.de',
+  ci: {
+    budget: {
+      performance: 50,
+      accessibility: 80,
+      'best-practices': 90,
+      seo: 90,
+    },
+  },
 }
