82189e4294a54d5d8530666b426863ef8269fdc7
Levin Keller <post@levinkeller.de>
2023-11-09 21:03:22 +0100

    feat(test): show failing redirects

--- redirects.test.ts
+++ redirects.test.ts
@@ -11,4 +11,20 @@ describe('Redirects', () => {
       expect(res.status).toEqual(301)
       expect(res.headers.get('location')).toEqual('https://www.kulturland.de/')
     }))
+  test('https://kulturland.de has correct certificate', () =>
+    expect(
+      fetch('https://kulturland.de', {
+        redirect: 'manual',
+      })
+    ).resolves.toBeTruthy())
+  test('https://kulturland.de to https://www.kulturland.de', () =>
+    fetch('https://kulturland.de', {
+      redirect: 'manual',
+      tls: {
+        rejectUnauthorized: false,
+      },
+    }).then((res) => {
+      expect(res.status).toEqual(301)
+      expect(res.headers.get('location')).toEqual('https://www.kulturland.de/')
+    }))
 })
